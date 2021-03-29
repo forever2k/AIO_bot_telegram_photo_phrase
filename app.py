e050afa94729d75b828dfa5b79cbbebfae0adedb
forever2k <forever2k@yandex.ru>
2021-03-29 21:56:18 +0300

    1

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ TOKEN = '1705182368:AAE4G_9-HB50SwVvTEJvLHEkWNLJ83kEaU4'
 
 TOKEN = '1705182368:AAE4G_9-HB50SwVvTEJvLHEkWNLJ83kEaU4'
 
-WEBHOOK_HOST = 'https://aio-bot-telegram-photo-phrase.herokuapp.com/'
-WEBHOOK_PATH = '/webhook/'
+WEBHOOK_HOST = 'https://aio-bot-telegram-photo-phrase.herokuapp.com'
+WEBHOOK_PATH = '/' + TOKEN
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 WEBAPP_HOST = '0.0.0.0'
