d484401ad8518966bcda9092bb9cfaed1b1e0638
forever2k <forever2k@yandex.ru>
2021-03-30 22:19:03 +0300

    1

--- app.py
+++ app.py
@@ -109,30 +109,30 @@ async def main_2(message : types.Message):
 
 
 
-async def on_startup():
-    await bot.delete_webhook()
-    await bot.set_webhook(WEBHOOK_URL)
+async def on_startup(dp):
+    logging.warning(
+        'Starting connection. ')
+    await bot.set_webhook(WEBHOOK_URL,drop_pending_updates=True)
 
 
-# Run before shutdown
-async def on_shutdown():
-    logging.warning("Shutting down..")
-    await bot.delete_webhook()
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-    logging.warning("Bot down")
+async def on_shutdown(dp):
+    logging.warning('Bye! Shutting down webhook connection')
 
 
-if __name__ == "__main__":
-    executor.start_webhook(
+def main():
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
         skip_updates=True,
+        on_startup=on_startup,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
+
+
+if __name__ == "__main__":
+    main()
 
 
 
